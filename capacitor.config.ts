c4a31205a74b740cdb82bec2719bc8767db9b363
mthesoliver <m.limaoliveira96@gmail.com>
2023-10-01 11:47:07 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'med-mais-v2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
