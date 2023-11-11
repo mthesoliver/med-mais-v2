4d7bd4da4def5fda8ac516b72faf5ad6e92c75a9
mthesoliver <m.limaoliveira96@gmail.com>
2023-11-11 17:05:43 -0300

    calendar ajustes

--- src/app/pages/home-feed/agendar-exames/calendar/mycalendar.component.ts
+++ src/app/pages/home-feed/agendar-exames/calendar/mycalendar.component.ts
@@ -76,7 +76,7 @@ export class MyCalendarComponent implements OnInit {
     const toAdd: CalEvent = {
       title: this.newEvent.title,
       startTime: new Date(this.newEvent.startTime),
-      endTime: new Date(this.newEvent.startTime.getHours() + 1),
+      endTime: new Date(this.newEvent.endTime),
       allDay: false
     };
     console.log(toAdd);
